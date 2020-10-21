74074a384829da7ffe21ef41d1c5a56badd0416a
carpdm-dev <sinnisingla@outlook.com>
2020-10-21 19:59:44 +0530

    Add compose post functionality and restrict length of individual posts on home page and provide rad more button. Read more button takes to next page.

--- app.js
+++ app.js
@@ -2,12 +2,12 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 
-const postOne = "Choose a stack and then, Practice! Practice! Practice!";
+const postOne = "Choose a stack and then, Practice! Practice! Practice!.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
 const app = express();
-const postList = [{title: "How to become a full-stack developer?", post: postOne}];
+const postList = [{postid: 1, title: "How to become a full-stack developer?", post: postOne}];
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
@@ -30,12 +30,25 @@ app.get("/compose", function(req, res) {
 });
 
 app.post("/compose", function(req, res) {
+    const id = postList.length + 1;
     const blogPost = {
         title: req.body.title,
-        post: req.body.post
+        post: req.body.post,
+        postid: id
     }
     postList.push(blogPost);
     res.redirect("/");
+});
+
+app.get("/posts/:postid", function(req, res) {
+    const postId = req.params.postid;
+    let postCurrent = {};
+    postList.forEach(function(post) {
+        if(post.postid == postId) {
+            postCurrent = post;
+        }
+    });
+    res.render("post",  {post: postCurrent});
 });
 
 
